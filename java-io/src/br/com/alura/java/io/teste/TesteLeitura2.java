daaa5675419e6997ff50a344ae163e4f11414fc8
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-06 19:06:47 -0300

    05 Formatação de valores - Aula 04 completa

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -8,7 +8,7 @@ public class TesteLeitura2 {
 
 	public static void main(String[] args) throws Exception {
 
-		Scanner scanner = new Scanner(new File("contas.csv"));
+		Scanner scanner = new Scanner(new File("java-io/contas.csv"));
 
 		while (scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
@@ -18,13 +18,14 @@ public class TesteLeitura2 {
 			linhaScanner.useLocale(Locale.US);
 			linhaScanner.useDelimiter(",");
 
-			String valor1 = linhaScanner.next();
-			int valor2 = linhaScanner.nextInt();
-			int valor3 = linhaScanner.nextInt();
-			String valor4 = linhaScanner.next();
-			double valor5 = linhaScanner.nextDouble();
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
 
-			String valorFormatador = String.format("%s %s %s %s %s", valor1, valor2, valor3, valor4, valor5);
+			String valorFormatador = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %s: %08.2f", tipoConta,
+					agencia, numero, titular, saldo);
 
 			System.out.println(valorFormatador);
 
