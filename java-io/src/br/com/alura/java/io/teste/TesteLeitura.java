83e1588608ee3fdbffe0f0c51d0fbd75bbb722fd
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-06 12:07:57 -0200

    04 InputStream e Reader - AULA 01

--- java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
+++ java-io/src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -3,22 +3,27 @@ package br.com.alura.java.io.teste;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
-		
-		//Fluxo de entrada com arquivo
-		FileInputStream fileInputStream = new FileInputStream("lorem.txt");
-		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-		
+
+		// Fluxo de entrada com arquivo
+		InputStream inputStream = new FileInputStream("lorem.txt");
+	    Reader reader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(reader);
+
 		String linha = bufferedReader.readLine();
-		
-		System.out.println(linha);
+
+		while (linha != null) {
+			System.out.println(linha);
+			linha = bufferedReader.readLine();
+		}
 		
 		bufferedReader.close();
-		
+
 	}
 }
